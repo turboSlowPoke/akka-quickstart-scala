bdf6296a32f050539d36317def1038579f13612c
i.kuteynikov <i.kuteynikov@telda.ru>
2018-11-27 17:51:25 +0300

    тест люцена

--- src/main/scala/com/example/lucen/LuceneActor.scala
+++ src/main/scala/com/example/lucen/LuceneActor.scala
@@ -4,32 +4,69 @@ import java.io.IOException
 import java.nio.file.{Path, Paths}
 
 import akka.actor.{Actor, ActorLogging, Props}
-import com.example.lucen.LuceneActor.{FindFireObjectsRequest, FoundFireObjectsResponse, UpdateOrCreateFireObject}
+import com.example.lucen.LuceneActor.{DeleteFireObject, FindFireObjectsRequest, FoundFireObjectsResponse, UpdateOrCreateFireObject}
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.document._
 import org.apache.lucene.index.IndexWriterConfig.OpenMode
 import org.apache.lucene.index.{DirectoryReader, IndexWriter, IndexWriterConfig, Term}
+import org.apache.lucene.queryparser.classic.QueryParser
 import org.apache.lucene.search._
 import org.apache.lucene.store.FSDirectory
 
 import scala.util.Try
-
-class LuceneActor(lucenDir:String) extends Actor with  ActorLogging{
-  private val path: Path = Paths.get(lucenDir)
+/**
+  * Актор для записи, обновления, поиска и удаления fireObject в apache Lucene
+   */
+class LuceneActor(luceneDir:String) extends Actor with  ActorLogging{
+  private val path: Path = Paths.get(luceneDir) //здесь будем писать и читать
   private val analyzer: StandardAnalyzer = new StandardAnalyzer()
-  private val initObject = FireObject(0,"name_00","type_00","rang1","prim","rang")
-
-
-//  private val searcher:IndexSearcher=Try(createSearcher(path,analyzer)).getOrElse(initLuceneDir())
-
+  private val initObject = FireObject(0,"object_00","type_00","rang1","prim","rang")
 
   override def preStart(): Unit = {
-    Try(DirectoryReader.open(FSDirectory.open(path))).getOrElse(initLuceneDir())
+    //если при открытии reader-а вылетает исключение, то возможно папка пуста и надо туда что-то записать
+    Try(DirectoryReader.open(FSDirectory.open(path)).close()).getOrElse(initLuceneDir())
   }
 
   private def initLuceneDir(): Unit = {
     addNewDoc(documentFromFireObject(initObject))
-    log.info(s"Директория Lucene: ${lucenDir} была проинициализирована")
+    log.info(s"Директория Lucene: $luceneDir была проинициализирована")
+  }
+
+  override def receive: Receive = {
+    case UpdateOrCreateFireObject(fireObject: FireObject) =>
+     search("gid_firepoints",fireObject.id.toString) match {
+        case Some(fireFromLucen) =>
+          if (fireFromLucen.length>1){
+            log.error("Дублирование объектов в Lucene, объект не будет обновляться: " + fireFromLucen)
+          }else{
+            update(documentFromFireObject(fireObject))
+            log.info("Обновлен объект: "+fireObject)
+          }
+        case None =>
+         addNewDoc(documentFromFireObject(fireObject))
+         log.info("Добавлен новый объект: " + fireObject)
+      }
+
+    case FindFireObjectsRequest(fieldName:String, keyWord:String) =>
+      search(fieldName,keyWord) match {
+        case Some(scoreDocs) =>
+          val reader = DirectoryReader.open(FSDirectory.open(path))
+          val searcher = new IndexSearcher(reader)
+          val docs: Array[Document] = scoreDocs.map(scoreDoc => searcher.doc(scoreDoc.doc))
+          reader.close()
+          val fireObjects: Array[FireObject] = docs.map(
+            doc => fireObjectFromDocument(doc)
+          )
+          sender() ! FoundFireObjectsResponse(fireObjects.toList)
+        case None =>
+          sender() ! FoundFireObjectsResponse(List.empty[FireObject])
+      }
+
+    case DeleteFireObject(fireObject: FireObject) =>
+      deleteDoc(fireObject.id.toString)
+      log.info("Удален объект: " +fireObject)
+    case _ =>
+      log.warning("Неизвестный запрос")
   }
 
   private def createWriter()={
@@ -39,63 +76,35 @@ class LuceneActor(lucenDir:String) extends Actor with  ActorLogging{
     new IndexWriter(directory, writerConfig)
   }
 
-  override def receive: Receive = {
-    case UpdateOrCreateFireObject(fireObject: FireObject) =>
-      //найти в базе
-     search("gid_firepoints",fireObject.id.toString) match {
-        case Some(fireFromLucen) =>
-          if (fireFromLucen.length>1){
-            log.error("Дублирование объектов в Lucene, объект не будет обновляться: " + fireFromLucen)
-          }else{
-            update(documentFromFireObject(fireObject))
-          }
-        case None =>
-         addNewDoc(documentFromFireObject(fireObject))
-      }
-    case FindFireObjectsRequest(fieldName:String, fieldValue:String) =>
-      search(fieldName,fieldValue) match {
-        case Some(scoreDocs) =>
-          val reader = DirectoryReader.open(FSDirectory.open(path))
-          val searcher = new IndexSearcher(reader)
-          val docs: Array[Document] = scoreDocs.map(scoreDoc => {searcher.doc(scoreDoc.doc)})
-          reader.close()
-          val fireObjects: Array[FireObject] = docs.map(
-            doc => fireObjectFromDocument(doc)
-          )
-          println(fireObjects.toList.head)
-          sender() ! FoundFireObjectsResponse(fireObjects.toList)
-        case None =>
-          sender() ! FoundFireObjectsResponse(List.empty[FireObject])
-      }
-
-    case _ =>
-      log.warning("Неизвестный запрос")
-  }
-
   private def update(doc: Document): Unit = {
     val writer = createWriter()
     writer.updateDocument(new Term("gid_firepoints",doc.get("gid_firepoints")),doc)
     writer.close()
   }
 
-  private def search(fieldName:String, fieldValue:String): Option[Array[ScoreDoc]] = {
-    val gidBuilder: BooleanQuery.Builder = new BooleanQuery.Builder
-    gidBuilder.add(new TermQuery(new Term(fieldName, fieldValue)), BooleanClause.Occur.MUST)
+  private def deleteDoc(gidFirepoinst: String): Unit = {
+    val writer = createWriter()
+    writer.deleteDocuments(new Term("gid_firepoints",gidFirepoinst))
+    writer.close()
+  }
+
+  private def search(fieldNameInDoc:String, fieldValue:String): Option[Array[ScoreDoc]] = {
+    val query = new QueryParser(fieldNameInDoc,analyzer).parse(fieldValue)
     val reader = DirectoryReader.open(FSDirectory.open(path))
     val searcher = new IndexSearcher(reader)
-    val TopDocs = searcher.search(gidBuilder.build(),10)
+    val TopDocs = searcher.search(query,10)
     val scoreDocs = TopDocs.scoreDocs
+    reader.close()
     if (scoreDocs!=null && scoreDocs.nonEmpty)
       Some(scoreDocs)
     else None
   }
 
+
   private def addNewDoc(doc: Document): Unit = {
     val writer = createWriter()
-
     try {
       writer.addDocument(doc)
-      writer.commit()
     }catch {
       case e:IOException =>
         log.error(e,"IOException при попытке сохранить document: " + doc)
@@ -106,21 +115,29 @@ class LuceneActor(lucenDir:String) extends Actor with  ActorLogging{
     }
   }
 
-  def documentFromFireObject(fireObject: FireObject):Document={
+  private def documentFromFireObject(fireObject: FireObject):Document={
     val doc = new Document
     doc.add(new StringField("gid_firepoints", fireObject.id.toString, Field.Store.YES))
-    doc.add(new StringField("geomX", fireObject.x.toString, Field.Store.YES))
-    doc.add(new StringField("geomY", fireObject.y.toString, Field.Store.YES))
     doc.add(new TextField("nameobject", fireObject.nameobject, Field.Store.YES))
     doc.add(new StringField("typeobject", fireObject.typeobject, Field.Store.YES))
     doc.add(new StringField("rang", fireObject.rang, Field.Store.YES))
     doc.add(new StringField("prim", fireObject.prim, Field.Store.YES))
     doc.add(new StringField("rangmodifiers", fireObject.rangmodifiers, Field.Store.YES))
     doc.add(new StringField("addteches", fireObject.addteches, Field.Store.YES))
+    if (Option(fireObject.isenabled).isDefined) doc.add(new StringField("isenabled",fireObject.isenabled.toString, Field.Store.YES))//isenabled
+    if (Option(fireObject.oldoid).isDefined) doc.add(new StringField("oldoid",fireObject.oldoid.toString, Field.Store.YES))//oldoid
+    doc.add(new StringField("geomX", fireObject.x.toString, Field.Store.YES))
+    doc.add(new StringField("geomY", fireObject.y.toString, Field.Store.YES))
+    if (fireObject.address!=null && fireObject.address.nonEmpty) doc.add(new StringField("address",fireObject.address, Field.Store.YES)) //address
+    if (fireObject.street!=null && fireObject.street.nonEmpty) doc.add(new StringField("street",fireObject.street, Field.Store.YES))//street
+    if (fireObject.house!=null && fireObject.house.nonEmpty) doc.add(new StringField("house",fireObject.house, Field.Store.YES))//house
+    if (fireObject.updater!=null && fireObject.updater.nonEmpty) doc.add(new StringField("updater",fireObject.updater, Field.Store.YES))//updater
+    //updwhen
     doc
   }
 
-  def fireObjectFromDocument(doc:Document): FireObject =FireObject(
+
+  private def fireObjectFromDocument(doc:Document): FireObject =FireObject(
     doc.get("gid_firepoints").toInt,
     doc.get("nameobject"),
     doc.get("typeobject"),
@@ -138,14 +155,14 @@ class LuceneActor(lucenDir:String) extends Actor with  ActorLogging{
     doc.get("updater")
 //    doc.get("updwhen")
   )
-
 }
 
 object LuceneActor {
   def props(lucenDirSearch:String) = Props(new LuceneActor(lucenDirSearch))
   final case class UpdateOrCreateFireObject(fireObject: FireObject)
-  final case class FindFireObjectsRequest(fieldName:String, fieldValue:String)
+  final case class FindFireObjectsRequest(fieldName:String, keyWord:String)
   final case class FoundFireObjectsResponse(fireObjects:List[FireObject])
+  final case class DeleteFireObject(fireObject: FireObject)
 }
 
 
--- src/test/scala/com/example/LuceneTest.scala
+++ src/test/scala/com/example/LuceneTest.scala
@@ -3,7 +3,7 @@ package com.example
 import akka.actor.ActorSystem
 import akka.testkit.{TestKit, TestProbe}
 import com.example.lucen.{FireObject, LuceneActor}
-import com.example.lucen.LuceneActor.{FindFireObjectsRequest, FoundFireObjectsResponse, UpdateOrCreateFireObject}
+import com.example.lucen.LuceneActor.{DeleteFireObject, FindFireObjectsRequest, FoundFireObjectsResponse, UpdateOrCreateFireObject}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.collection.immutable
@@ -21,30 +21,48 @@ class LuceneTest(_system: ActorSystem)
       shutdown(system)
     }
 
-  "Есди запросить несуществующий объект должен вернуть пустой список" in {
+
+  "CRUD тест" in {
     val requester = TestProbe()
     val probe = TestProbe()
     val lucenActorRef = system.actorOf(LuceneActor.props(GlobalTesterConfig.LuceneDirectory))
-    lucenActorRef.tell(FindFireObjectsRequest("gid_firepoints","101"),probe.ref)
-    val response: FoundFireObjectsResponse = probe.expectMsgType[FoundFireObjectsResponse]
-    response.fireObjects.isEmpty should === (true)
-  }
+    val fireObject = FireObject(1,"object1")
+    val fireObjectUpdate = FireObject(1,"updated_object1")
 
-  "Записать два объекта с одинаковым id, при запросе для данного id должен вернуть один объект" in {
-    val requester = TestProbe()
-    val probe = TestProbe()
-    val lucenActorRef = system.actorOf(LuceneActor.props(GlobalTesterConfig.LuceneDirectory))
-    val fireObject = FireObject(11,"name01")
-    val fireObjectUpdate = FireObject(11,"updateName01")
+//    lucenActorRef.tell(FindFireObjectsRequest("gid_firepoints","101"),probe.ref)
+//    val response1: FoundFireObjectsResponse = probe.expectMsgType[FoundFireObjectsResponse]
+//    response1.fireObjects.isEmpty should === (true)
+//
+//    lucenActorRef.tell(UpdateOrCreateFireObject(fireObject),probe.ref)
+//    lucenActorRef.tell(FindFireObjectsRequest("gid_firepoints",fireObject.id.toString),probe.ref)
+//
+//    val response2: FoundFireObjectsResponse = probe.expectMsgType[FoundFireObjectsResponse]
+//    response2.fireObjects.length should === (1)
+//    response2.fireObjects.head.id should === (fireObject.id)
+//    response2.fireObjects.head.nameobject should === (fireObject.nameobject)
+//
+//    lucenActorRef.tell(UpdateOrCreateFireObject(fireObjectUpdate),probe.ref)
+//    lucenActorRef.tell(FindFireObjectsRequest("gid_firepoints",fireObject.id.toString),probe.ref)
+//    val response3: FoundFireObjectsResponse = probe.expectMsgType[FoundFireObjectsResponse]
+//    response3.fireObjects.length should === (1)
+//    response3.fireObjects.head.id should === (fireObjectUpdate.id)
+//    response3.fireObjects.head.nameobject should === (fireObjectUpdate.nameobject)
+//
+//    lucenActorRef.tell(DeleteFireObject(fireObject),probe.ref)
+//    lucenActorRef.tell(FindFireObjectsRequest("gid_firepoints",fireObject.id.toString),probe.ref)
+//    val response4: FoundFireObjectsResponse = probe.expectMsgType[FoundFireObjectsResponse]
+//    response4.fireObjects.isEmpty should === (true)
 
-    lucenActorRef.tell(UpdateOrCreateFireObject(fireObject),probe.ref)
-    lucenActorRef.tell(UpdateOrCreateFireObject(fireObjectUpdate),probe.ref)
-    lucenActorRef.tell(FindFireObjectsRequest("gid_firepoints",fireObject.id.toString),probe.ref)
+    val fireObject1 = FireObject(1,"auto nameee #35")
+    val fireObject2 = FireObject(2,"big nameee lalala")
+    val fireObject3 = FireObject(3,"mega nameeelkddsds")
 
-    val response: FoundFireObjectsResponse = probe.expectMsgType[FoundFireObjectsResponse]
-    response.fireObjects.length should === (1)
-    response.fireObjects.head.id should === (fireObjectUpdate.id)
-    response.fireObjects.head.nameobject should === (fireObjectUpdate.nameobject)
+    lucenActorRef.tell(UpdateOrCreateFireObject(fireObject1),probe.ref)
+    lucenActorRef.tell(UpdateOrCreateFireObject(fireObject2),probe.ref)
+    lucenActorRef.tell(UpdateOrCreateFireObject(fireObject3),probe.ref)
+    lucenActorRef.tell(FindFireObjectsRequest("nameobject","nameee*"),probe.ref)
+    val response5: FoundFireObjectsResponse = probe.expectMsgType[FoundFireObjectsResponse]
+    response5.fireObjects.length should === (3)
 
   }
 
